a55586597f1caba68bb6c52f9b15c41411e16180
han_taejun <plushan98@naver.com>
2024-02-22 20:12:58 +0900

    EventDto 구현(입력값 제한하기 구현)

--- src/main/java/me/hantomas/restapi/events/EventController.java
+++ src/main/java/me/hantomas/restapi/events/EventController.java
@@ -1,5 +1,6 @@
 package me.hantomas.restapi.events;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,11 +17,17 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 public class EventController {
     private final EventRepository eventRepository;
 
-    public EventController(EventRepository eventRepository){
+    private final ModelMapper modelMapper;
+
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper){
         this.eventRepository = eventRepository;
+        this.modelMapper = modelMapper;
     }
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event){
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto){
+
+        Event event = modelMapper.map(eventDto, Event.class);
+
         Event newEvent = this.eventRepository.save(event);
 
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
