34ef0b955d7bb429892a525a7bdf7013ecb069ac
han_taejun <plushan98@naver.com>
2024-02-26 18:51:44 +0900

    비즈니스 로직 구현, 매개변수를 이용한 테스트 구현

--- src/test/java/me/hantomas/restapi/events/EventTest.java
+++ src/test/java/me/hantomas/restapi/events/EventTest.java
@@ -1,9 +1,12 @@
 package me.hantomas.restapi.events;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
     @Test
     public void builder(){
@@ -29,5 +32,51 @@ public class EventTest {
         // Then
         assertThat(event.getName()).isEqualTo(name);
         assertThat(event.getDescription()).isEqualTo(description);
+    }
+
+    @Test
+    @Parameters(method = "paramsForTestFree")
+    public void testFree(int basePrice, int maxPrice, boolean isFree){
+        // Given
+        Event event = Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
+                .build();
+        // When
+        event.update();
+
+        // Then
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
+
+    private Object[] paramsForTestFree(){
+        return new Object[]{
+                new Object[] {0, 0, true},
+                new Object[] {100, 0, false},
+                new Object[] {0, 100, false},
+                new Object[] {100, 200, false}
+        };
+    }
+
+    @Test
+    @Parameters(method = "parametersForTestOffline")
+    public void  testOffline(String location, boolean isOffline){
+        // Given
+        Event event = Event.builder()
+                .location(location)
+                .build();
+        // When
+        event.update();
+
+        // Then
+        assertThat(event.isOffline()).isEqualTo(isOffline);
+    }
+
+    private Object[] parametersForTestOffline(){
+        return new Object[]{
+                new Object[]{"강남", true},
+                new Object[]{null, false},
+                new Object[]{"       ", false}
+        };
     }
 }
