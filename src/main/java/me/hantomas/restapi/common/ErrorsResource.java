0725f5a58e0d2776cd128f82a2f4262e28105d54
han_taejun <plushan98@naver.com>
2024-03-04 19:02:38 +0900

    API 인덱스 만들기

---
+++ src/main/java/me/hantomas/restapi/common/ErrorsResource.java
@@ -0,0 +1,17 @@
+package me.hantomas.restapi.common;
+
+import me.hantomas.restapi.index.IndexController;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.validation.Errors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+public class ErrorsResource extends Resource<Errors> {
+
+    public ErrorsResource(Errors errors, Link... links) {
+        super(errors, links);
+        add(linkTo(methodOn(IndexController.class).index()).withRel("index"));
+    }
+}
