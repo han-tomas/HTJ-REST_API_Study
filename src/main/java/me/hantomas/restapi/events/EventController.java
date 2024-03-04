93cf9dc4f02f5b001efa9ee8cffabc3e4165763b
han_taejun <plushan98@naver.com>
2024-03-04 21:17:24 +0900

    Event 목록 조회 API 구현

--- src/main/java/me/hantomas/restapi/events/EventController.java
+++ src/main/java/me/hantomas/restapi/events/EventController.java
@@ -2,12 +2,18 @@ package me.hantomas.restapi.events;
 
 import me.hantomas.restapi.common.ErrorsResource;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,5 +63,14 @@ public class EventController {
     }
     private  ResponseEntity badRequest(Errors errors){
         return ResponseEntity.badRequest().body(new ErrorsResource(errors));
+    }
+
+    @GetMapping
+    public ResponseEntity queryEvents(Pageable pageable, PagedResourcesAssembler<Event> assembler) {
+        Page<Event> page = this.eventRepository.findAll(pageable);
+        var pagedResources = assembler.toResource(page, e -> new EventResource(e));
+        pagedResources.add(new Link("/docs/index.html#resources-events-list").withRel("profile"));
+        //PagedResources<Resource<Event>> pageResources = assembler.toResource(page);
+        return ResponseEntity.ok(pagedResources);
     }
 }
